343e55c03ed3e2bd5cb1d3a6b56a907b859a2f53
gupta-nu <gupta.ananya@protonmail.com>
2024-10-02 13:37:05 +0530

    static middelware issues resolved

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express= require('express');
+const morgan = require('morgan');
 
 const app= express();
 
 //register view engine
 app.set('view engine', 'ejs');
-//to create seperate folder to store ejs files
-app.set('vies','ejsviews');
+
+
 //listen for requests
 
 app.listen(3000);
+//middleware and static files 
+app.use(express.static("public"));
+
+app.use(morgan('dev'));
 
 app.get('/',(req,res)=>
     {
